4f813857d913979860535ec931864a3233aa44c8
Rei Sturm <reisturm@Reis-MacBook-Pro.local>
2019-12-03 16:45:34 -0800

    search bar early implementation

--- src/App.js
+++ src/App.js
@@ -141,7 +141,7 @@ class App extends Component {
             </div>
             {/* <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>Bill is a cat.</div> */}
             <WrappedMap
-              googleMapURL={`https://maps.googleapis.com/maps/api/js?key=AIzaSyB6C4COk_CiplZhE0Mn4ZS76_zFoSUP1hU`}
+              googleMapURL={`https://maps.googleapis.com/maps/api/js?key=***API_KEY_HERE***`}
               loadingElement={<div style={{ height: `100%` }} />}
               containerElement={<div style={{ height: `100%` }} />}
               mapElement={<div style={{ height: `100%` }} />}
