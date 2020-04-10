66d208815c3d3ac733b98f6485e5b044658998a3
lzhang100 <im.mr.lester@gmail.com>
2020-04-09 18:52:03 -0700

    fixedsearchbar

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -3,26 +3,30 @@ import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   inputRoot: {
     color: 'black',
     background: 'white',
     '& .MuiOutlinedInput-notchedOutline': {
-      borderColor: 'white'
+      borderColor: 'white',
     },
     '&:hover .MuiOutlinedInput-notchedOutline': {
-      borderColor: 'white'
-    }
-  }
+      borderColor: 'white',
+    },
+  },
 }));
 
 export default function Searchbar2(props) {
   const handleChange = (event, values) => {
-    const value = event.target.innerHTML;
+    var value = event.target.innerHTML;
+    if (event.target.innerText === undefined) {
+      value = '';
+      event.target.value = '';
+    }
     if (value === '') {
       const inputAcro = event.target.value.toLowerCase();
-      if (props.buildings.filter(b => b.acro.toLowerCase() === inputAcro).length !== 0) {
-        const building = props.buildings.filter(b => b.acro.toLowerCase() === inputAcro)[0];
+      if (props.buildings.filter((b) => b.acro.toLowerCase() === inputAcro).length !== 0) {
+        const building = props.buildings.filter((b) => b.acro.toLowerCase() === inputAcro)[0];
         event.target.innerHTML = building.name;
         props.onSearchClicked(building.center);
         props.clickPolygon(building.name);
@@ -31,7 +35,7 @@ export default function Searchbar2(props) {
       return;
     }
 
-    const building = props.buildings.filter(b => b.name === value)[0];
+    const building = props.buildings.filter((b) => b.name === value)[0];
     event.target.innerHTML = building.name;
     props.onSearchClicked(building.center);
     props.clickPolygon(building.name);
@@ -46,9 +50,9 @@ export default function Searchbar2(props) {
         freeSolo
         color='primary'
         classes={classes}
-        options={props.buildings.map(b => b.name).sort()}
+        options={props.buildings.map((b) => b.name).sort()}
         onChange={handleChange}
-        renderInput={params => (
+        renderInput={(params) => (
           <TextField {...params} label='Search...' margin='normal' variant='outlined' />
         )}
       />
